8c0de40cd5235320ecd822386cd4167990521e07
Ciro Giordano <95182@sistemas.frc.utn.edu.ar>
2026-01-07 17:58:35 -0300

    DataTypes and Variables

---
+++ week-01-basics/src/main/java/com/ciro/practice/DataTypes.java
@@ -0,0 +1,25 @@
+package com.ciro.practice;
+
+public class DataTypes {
+
+    public static void main(String[] args){
+
+        // Tipos de datos primitivos
+
+        int myInt = 37;
+        System.out.println(myInt);
+
+        double myDouble = 1.77;
+        System.out.println(myDouble);
+
+        char myChar = 'A';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        myBoolean = false;
+        System.out.println(myBoolean);
+
+        //float, long, byte
+
+    }
+}
---
+++ week-01-basics/src/main/java/com/ciro/practice/VariablesAndConstants.java
@@ -0,0 +1,25 @@
+package com.ciro.practice;
+
+public class VariablesAndConstants {
+
+    public static void main(String[] args){
+
+        // Variables
+
+        String name = "Brian";
+        System.out.println(name);
+
+        name = "Mauro";
+        System.out.println(name);
+
+        int age = 40;
+        System.out.println(age);
+
+        // Constantes van con mayus como buena practica
+
+        final String EMAIL = "abc123@gmail.com";
+        System.out.println(EMAIL);
+
+
+    }
+}
